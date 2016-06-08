47e630a6109ee395aedfa9fd2a62267f327d786a
Erick Garcia <erickarmandogarcia@hotmail.com>
2016-06-08 15:52:48 -0500

    Add parent and child component into project.

--- src/app/angular2-first-steps.component.ts
+++ src/app/angular2-first-steps.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MovimientoComponent } from './movimiento/movimiento.component'
 
 @Component({
   moduleId: module.id,
   selector: 'angular2-first-steps-app',
   templateUrl: 'angular2-first-steps.component.html',
-  styleUrls: ['angular2-first-steps.component.css']
+  styleUrls: ['angular2-first-steps.component.css'],
+  directives:[MovimientoComponent]
 })
 export class Angular2FirstStepsAppComponent {
   title = 'angular2-first-steps works!';
---
+++ src/app/movimiento/movimiento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-movimiento',
+  templateUrl: 'movimiento.component.html',
+  styleUrls: ['movimiento.component.css']
+})
+export class MovimientoComponent {
+  constructor() {}
+}
